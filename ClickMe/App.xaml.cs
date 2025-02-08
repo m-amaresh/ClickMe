199ed000a3563f868ea69c745e2ce9c9ea09b807
Amaresh Mohapatra <m.amaresh@hotmail.com>
2025-02-08 23:08:32 +0530

    Initial Commit

---
+++ ClickMe/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ClickMe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ClickMe/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ClickMe
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
